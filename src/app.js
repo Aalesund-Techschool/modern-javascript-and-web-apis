628737b87cbd6bea0db4f3690f19eeae072e1282
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2025-03-21 08:56:49 +0100

    Logout server listening port

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import http from 'http';
-import routes from './backend/routes.js';
+import express from "express";
+import http from "http";
+import routes from "./backend/routes.js";
 
 const app = express();
 const port = 3000;
 
-app.set('port', process.env.PORT || port)
-app.use(express.static('frontend'));
+app.set("port", process.env.PORT || port);
+app.use(express.static("frontend"));
 app.use(routes);
 
-
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(port);
+console.log(`🔌 Server is running on http://localhost:${port}`);
